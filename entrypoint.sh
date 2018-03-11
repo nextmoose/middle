921ca173b29857e7582d1ec82a4502fda60c2822
Emory Merryman <emory.merryman@gmail.com>
2018-03-11 22:18:50 +0000

    preparation commit

--- entrypoint.sh
+++ entrypoint.sh
@@ -54,7 +54,7 @@ cleanup(){
         --env TARGET_UID \
         --env XDG_RUNTIME_DIR=/var/run/${TARGET_UID} \
         --mount type=bind,source=/srv/pulse,target=/run/user/${TARGET_UID}/pulse,readonly=false \
-        --mount type=bind,source=/home/user/workspace,destination=/home/user/workspace,readonly=false \
+        --mount type=bind,source=/opt/cloud9/workspace,destination=/opt/cloud9/workspace,readonly=false \
         --mount type=bind,source=/srv/host/tmp/.X11-unix,destination=/tmp/.X11-unix,readonly=true \
         --mount type=bind,source=/srv/host/var/run/docker.sock,destination=/var/run/docker.sock,readonly=true \
         --mount type=bind,source=/srv,destination=/srv,readonly=false \
