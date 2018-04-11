5254d772d2d74f73a73cddbc246a7c0d0dd5eadb
Emory Merryman <emory.merryman@gmail.com>
2018-04-11 11:22:40 +0000

    We will use a volume to cache the browser. When we restart the browser will have custom setting from the last time. If we add a bookmark it will still be there next time. The same for extensions.
    To keep the volumes from growing out of hand,
    we will add an expiry label to each volume and prune expired volumes
    at the end of each session.
    
    If a volume is not expired, then it does not get pruned.

--- entrypoint.user.sh
+++ entrypoint.user.sh
@@ -3,18 +3,48 @@
 cleanup(){
     /usr/local/bin/docker container stop browser inner &&
         /usr/local/bin/docker container prune --force &&
+        /usr/local/bin/docker container prune --force &&
         /usr/local/bin/docker network prune --force &&
-        /usr/local/bin/docker volume prune --force
+        /usr/local/bin/docker volume ls --filter label=expiry | while read VOLUME
+        do
+            [ $(/usr/local/bin/docker volume inspect --format "{{ .Labels.expiry }}" ${VOLUME}) -lt $(date +%s) ] &&
+                /usr/local/bin/docker volume rm ${VOLUME}
+        done
 } &&
     trap cleanup EXIT &&
     /usr/local/bin/docker container prune --force &&
     /usr/local/bin/docker network prune --force &&
-    /usr/local/bin/docker volume prune --force &&
-    /usr/local/bin/docker \
+NEW_DATA_VOLUME=$(/usr/bin/docker volume create --label expiry=$(date --date "now + 1 week" +%s) --label moniker=c707b6ae-93ca-4f2f-be64-9c45508a72cb) &&
+    /usr/bin/docker volume ls --quiet --filter label=moniker=c707b6ae-93ca-4f2f-be64-9c45508a72cb | while read VOLUME
+    do
+        /usr/bin/docker volume inspect --format "XXX {{ .Labels.expiry }} ${VOLUME} XXX" ${VOLUME}
+    done | sort -n -k 1 -r &&
+    /usr/bin/docker volume ls --quiet --filter label=moniker=c707b6ae-93ca-4f2f-be64-9c45508a72cb | while read VOLUME
+    do
+        /usr/bin/docker volume inspect --format "{{ .Labels.expiry }} ${VOLUME}" ${VOLUME}
+    done | sort -n -k 1 -r | cut --fields 2 --delimiter " " | head --lines 2 | tail --lines 1 | while read OLD_DATA_VOLUME
+    do
+        [ ${NEW_DATA_VOLUME} != ${OLD_DATA_VOLUME} ] &&
+            /usr/bin/docker \
+                container \
+                run \
+                --interactive \
+                --rm \
+                --label expiry=$(date --date "now + 1 hour" +%s) \
+                --volume ${OLD_DATA_VOLUME}:/input:ro \
+                --volume ${NEW_DATA_VOLUME}:/output \
+                alpine:3.4 \
+                    cp \
+                        -rp \
+                        /input/. \
+                        /output
+    done
+    docker \
         container \
         create \
         --name browser \
         --privileged \
+        --mount type=volume,source=${NEW_DATA_VOLUME},destination=/data,readonly=false \
         --mount type=bind,source=/srv/host/tmp/.X11-unix,destination=/tmp/.X11-unix,readonly=true \
         --mount type=bind,source=/srv/pulse,destination=/run/user/${TARGET_UID}/pulse,readonly=false \
         --mount type=bind,source=/srv/machine-id,destination=/etc/machine-id,readonly=false \
@@ -27,7 +57,7 @@ cleanup(){
         --env TARGET_UID="${TARGET_UID}" \
         --env XDG_RUNTIME_DIR=/run/user/${TARGET_UID} \
         urgemerge/chromium-pulseaudio@sha256:21d8120ff7857afb0c18d4abf098549de169782e652437441c3c7778a755e46f \
-            http://inner:10604 &&
+            http://inner:10604
     /usr/local/bin/docker \
         container \
         create \
